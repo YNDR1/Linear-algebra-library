7781e544f30b12979987b1abd1d225989aa46ea6
YNDR1 <69757325+YNDR1@users.noreply.github.com>
2021-01-16 17:06:27 +0300

    Add files via upload

---
+++ Linear-algebra-library/lal.h
@@ -0,0 +1,38 @@
+#define lal_h_
+#include <iostream>
+
+using namespace std;
+
+
+
+class Matris{
+	
+	public:
+		double *entries;
+		int rows;
+		int cols;
+	Matris(double *, int,int);
+	~Matris(){};
+	void print(void);
+	Matris operator+(Matris);
+	Matris operator-(Matris);
+	Matris operator*(Matris);
+	Matris operator==(Matris);
+	
+	};
+
+
+class Vektor: public Matris{
+
+	public:
+	double norm;
+	 
+	 Vektor(double *, int,int): Matris(entries,rows,cols){};
+	 ~Vektor(){};
+	 void print(void);
+	 };
+	 
+
+void matrisDoldur(Matris *);
+
+ 
---
+++ Linear-algebra-library/test.cpp
@@ -0,0 +1,9 @@
+#include "lal.h"
+
+int main(){
+    double *a ;
+	Matris *m = new Matris(a,5,5);
+//	matrisDoldur(m);
+	
+	
+}
