5c66bc572e6d7597583f8e1b045c653919702301
YNDR1 <69757325+YNDR1@users.noreply.github.com>
2021-01-16 23:19:39 +0300

    Update lal.cpp

--- Linear-algebra-library/lal.cpp
+++ Linear-algebra-library/lal.cpp
@@ -1,87 +1,107 @@
 #include "lal.h"
 #include <iostream>
+#include "math.h"
+
+using namespace std;
 
 
-class Matris{
+Matris::Matris(double *e, int r,int c){
+this->entries= e;
+this->rows = r;
+this->cols = c;
+}
 
-	public:
-		double *entries;
-		int rows;
-		int cols;
-		
-	Matris::Matris(double *e, int r,int c){
-	this->entries = e; 
-	this->rows = r;
-	this->cols = c;
-	};
-	
-	~Matris(){};	
-	
-	void print(void){
-		int i,j;
-		for(i = 0; i<this.rows; i++){
-		for(j = 0; j < this.cols; j++){
-			cout << this.entries[i][j] << endl;
-		}
-		}
-	}
-	
+Matris::~Matris(){
+cout<< "Matris has been deleted" << endl;
 
-	Matris::operator+(Matris m){
-		int i,j;
-		for(i = 0; i < m.rows; i++){
-			for(j = 0; j < m.cols; j++){
-				rows += m[i][j];
-				cols += m[i][j];
-			}
-		}
-	}
-	Matris::operator-(Matris m){
-			int i,j;
-		for(i = 0; i < m.rows; i++){
-			for(j = 0; j < m.cols; j++){
-				rows -= m[i][j];
-				cols -= m[i][j];
-			}
-		}
-	}
-	Matris::operator*(Matris m){
-	    int i,j;
-		for(i = 0; i < m.rows; i++){
-			for(j = 0; j < m.cols; j++){
-				rows *= m[i][j];
-				cols *= m[i][j];
-			}
-		}
-	}
-	Matris::operator==(Matris m){
-		
-	}
-	
-	
-	
-};
-class Vektor: public Matris{
+}
+void Matris::getEntires(){
+return this->entries;
+}
+void Matris::getRows(){
+return this->rows;
+}
+void Matris::getCols(){
+return this->cols;
+}
+void Matris::print() {
+double **matrix =(double **)malloc(this->rows*sizeof(double *));
+          int i,j;
 
-	
-	public:
-		double norm;
-	 Vektor(double *d, int rows): Matris{d,rows};
-     
-     
-	 ~Vektor(){};
-	 void print(void){
-	 	int i;
-	 	for(i = 0; i < rows; i++){
-	 		cout << (d[i]) << endl;
-		 }
-	 }	 
-};
+          for (i=0; i<this->rows; i++) {
+                    matrix[i] = (double *)malloc(this->cols * sizeof(double));
+          }
+
+          for(i=0; i<this->rows; i++) {
+                    for(j=0; j<this->cols; j++) {
+                              matrix[ i ][ j ] = this->entries[ i*this->cols + j ];
+                    }
+          }
+
+          for(i=0; i<this->rows; i++) {
+                    for(j=0; j<this->cols; j++) {
+                              cout << matrix[i][j] << endl;
+                    }
+
+                    puts("");
+          }
+}
+
+
+// Matris::operator+(Matris m){
+// int i,j;
+// for(i = 0; i < m.rows; i++){
+// for(j = 0; j < m.cols; j++){
+// rows += m[i][j];
+// cols += m[i][j];
+// }
+// }
+// }
+// Matris::operator-(Matris m){
+// int i,j;
+// for(i = 0; i < m.rows; i++){
+// for(j = 0; j < m.cols; j++){
+// rows -= m[i][j];
+// cols -= m[i][j];
+// }
+// }
+// }
+// Matris::operator*(Matris m){
+//    int i,j;
+// for(i = 0; i < m.rows; i++){
+// for(j = 0; j < m.cols; j++){
+// rows *= m[i][j];
+// cols *= m[i][j];
+// }
+// }
+// }
+// Matris::operator==(Matris m){
+// int i,j;
+// for(i = 0; i < m.rows; i++){
+// for(j = 0; j < m.cols; j++){
+// rows += m[i][j];
+// cols += m[i][j];
+// }
+// }
+// }
+
+
+
+Vector::Vector(double *d,int row):Matris(d,row,1)  {
+                    int i,sum=0;
+                    for(i=0; i<row; i++) {
+                              sum+=d[i]*d[i];
+                    }
+                    this->norm=sqrt(sum);
+}
+Vector::~Vector() {
+          cout<<"Vector has been deleted"<<endl;
+}
 void matrisDoldur(Matris *m){
-	int i, j;
-	for(i = 0; i < m.rows; i++) {
-		for(j = 0; j < m.cols; j++){
-			m[i][j] = rand()%3;
-		}
-	}
+// double *entries = (double *) malloc((m->getCols() * m->getRows()) * sizeof(double));
+// int i, j;
+// for(i = 0; i < m->getEntires(); i++) {
+// m[i] = rand()%10;
+// }
 };
+
